94a88aa469f39fc8d3bb25df65c7c9f413232f0b
adarsharyan002 <adarsharyanmuz@gmail.com>
2023-12-09 13:41:31 +0530

    minor changes

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const usersRouter = require('./routes/user');
 var cors = require('cors')
 
@@ -8,14 +7,14 @@ const app = express();
 const port = 3000;
 
 // Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.json());
+
 app.use(cors())
 
 // Routes
 app.use('/users', usersRouter);
 
-// Start the server
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
